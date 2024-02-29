3dbe5620254d05b0d933070d04697ab651cdb1b9
sadensmol <sadensmol@gmail.com>
2024-02-29 20:49:24 +0400

    initial thoughts

---
+++ cmd/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/sadensmol/test_playoff/internal"
+
+func main() {
+
+	internal.NewApp().Run()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sadensmol/test_playoff
+
+go 1.21.5
